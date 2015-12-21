d2f67a826418f261a4fa022d432f3f217f91ef07
Jan Hrdina <jan.hrdka@gmail.com>
2015-12-21 11:23:16 +0100

    New elements, gitignore updated
     - Section header
     - Construct cell
     - Toolbar prepared
     - .gitignore JS files

---
+++ app/elements/al-construct-cell/al-construct-cell.ts
@@ -0,0 +1,29 @@
+/// <reference path="../../../bower_components/polymer-ts/polymer-ts.d.ts"/>
+
+@component('al-construct-cell')
+class AlConstructCell extends polymer.Base {
+  @property({ type: String, notify: true })
+  text: String;
+
+  addConstructBelowTapped() {
+    this.fire('add-construct-below');
+  }
+
+  deleteTapped() {
+    this.fire('delete');
+  }
+
+  addSectionBelowTapped() {
+    this.fire('add-section-below');
+  }
+
+  moveUpTapped() {
+    this.fire('move-up');
+  }
+
+  moveDownTapped() {
+    this.fire('move-down');
+  }
+}
+
+AlConstructCell.register();
