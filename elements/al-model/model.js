0e20f491c823c42f37ee972ec077521cffca5633
Jan Hrdina <jan.hrdka@gmail.com>
2016-05-31 11:13:18 +0200

    Update 2016-05-31T11:13:18+02:00

--- elements/al-model/model.js
+++
@@ -1,9 +0,0 @@
-var Model = (function () {
-    function Model() {
-    }
-    Model.prototype.construct = function (json) {
-        this.languages = new LanguagesCollection();
-        this.constructs = [];
-    };
-    return Model;
-})();
--- scripts/app.js
+++
@@ -1,1 +0,0 @@
-!function(a){"use strict";var e=a.querySelector("#app");e.baseUrl="/",""===window.location.port&&(e.baseUrl="/alllangs/")}(document);
