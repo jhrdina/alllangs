0884e979ea621942749d1a739c1063ddc7e05a80
Jan Hrdina <jan.hrdka@gmail.com>
2016-05-31 09:14:51 +0200

    Import/Export functionality

--- src/al-app/al-app.ts
+++ src/al-app/al-app.ts
@@ -210,6 +210,50 @@ reader.readAsText(fileInput.files[0]);`
       prism: 'javascript'
     },
   ];
+
+  _onFileSelected(e) {
+    var file = e.target.files[0];
+    if (!file) {
+      return;
+    }
+
+    var reader = new FileReader();
+    reader.addEventListener('load', e => {
+      var data: any = e.target;
+      var model = JSON.parse(data.result);
+      this.sections = model.sections;
+      this.languages = model.languages;
+    });
+
+    reader.readAsText(file);
+  }
+
+  exportTapped() {
+    var data = JSON.stringify({
+      sections: this.sections,
+      languages: this.languages
+    }, null, 2)
+    this.download('data.json', data);
+  }
+
+  download(filename: string, data: string) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(data));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+
+    // TODO: Append to local DOM
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  }
+
+  importTapped() {
+    this.$.file.click();
+  }
 }
 
 AlApp.register();
