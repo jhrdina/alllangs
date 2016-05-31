3e4e0d51e889678a8ac6132877b7f9bbe777e631
Jan Hrdina <jan.hrdka@gmail.com>
2016-05-31 13:00:34 +0200

    Offline support, deploy script improved

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-polymer build || { echo 'Polymer Build failed'; exit 1; }
+tsc
+
+set -e
+
+if [ "$1" != "nobuild" ] ; then
+  rm -rf build
+  polymer build || { echo 'Polymer Build failed'; exit 1; }
+fi
 
 DIST_DIR="`pwd`/build/final"
-FILES=
+GH_PAGES_DIR="`pwd`/.gh"
 
 
 rm -rf "$DIST_DIR"
@@ -17,4 +24,15 @@ CNAME \
 index.html \
 manifest.json \
 service-worker.js \
-sw-precache-config.js
+sw-precache-config.js \
+bower_components/app-storage/app-indexeddb-mirror/app-indexeddb-mirror-worker.js
+
+
+#git clone -b gh-pages https://github.com/jhrdina/alllangs.git .
+cd "$GH_PAGES_DIR"
+git pull
+rm -rf *
+cp -rf "$DIST_DIR/"* -t "$GH_PAGES_DIR"
+
+git add --all
+git commit -m "Update `date -Iseconds`"
