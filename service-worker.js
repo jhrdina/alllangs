c447e4efd9682b47d074247883fd25a95a2847ce
Jan Hrdina <jan.hrdka@gmail.com>
2016-12-07 13:09:02 +0100

    Update 2016-12-07T13:09:02+01:00

--- service-worker.js
+++ service-worker.js
@@ -29,7 +29,7 @@
 
 
 /* eslint-disable quotes, comma-spacing */
-var PrecacheConfig = [["/bower_components/webcomponentsjs/webcomponents-lite.min.js","f04ed23700daeb36f637bfe095960659"],["/index.html","af39b3aaa7d7a944713010b7b4272050"],["/manifest.json","f71d123bd6e00895c523e90ff42159f8"],["/src/al-app/al-app.html","4b18ad2010ea94e818753b0847a8756f"]];
+var PrecacheConfig = [["/bower_components/webcomponentsjs/webcomponents-lite.min.js","f04ed23700daeb36f637bfe095960659"],["/index.html","af39b3aaa7d7a944713010b7b4272050"],["/manifest.json","f71d123bd6e00895c523e90ff42159f8"],["/src/al-app/al-app.html","1faca3ebfbd0ae86b304a7341ceece01"]];
 /* eslint-enable quotes, comma-spacing */
 var CacheNamePrefix = 'sw-precache-v1--' + (self.registration ? self.registration.scope : '') + '-';
 
