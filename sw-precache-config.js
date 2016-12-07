6749f5f6e9fb95fa7b7942bef2323673f8e1ac74
Jan Hrdina <jan.hrdka@gmail.com>
2016-12-07 14:24:20 +0100

    Enable data caching, make SW work

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -11,7 +11,8 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/bower_components/app-storage/app-indexeddb-mirror/app-indexeddb-mirror-worker.js'
   ],
   navigateFallback: '/index.html'
 };
